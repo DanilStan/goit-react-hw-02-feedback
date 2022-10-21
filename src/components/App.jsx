7147ffe346e4a2b89b3d2eb740ba0f0115c85f80
DanilStan <stanislavov.danik@gmail.com>
2022-10-21 17:21:55 +0300

    Update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,3 @@
-import
-
 export const App = () => {
   return (
     <div
@@ -9,7 +7,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
